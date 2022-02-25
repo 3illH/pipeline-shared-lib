79d1c9db945ad80cceeb64a8139cc3147af07b12
3ill <guiller.n93@gmail.com>
2022-02-25 07:51:44 +0100

    refactor project pt4

--- src/org/shared/utils/Checkout.groovy
+++ src/org/shared/utils/Checkout.groovy
@@ -2,7 +2,7 @@ package org.shared.utils
 
 class Checkout implements Serializable {
   def steps
-  Utilities(steps) {this.steps = steps}
+  Checkout(steps) {this.steps = steps}
   def checkout(args) {
     steps.checkout([$class: 'GitSCM', branches: [[name: 'master']], extensions: [], userRemoteConfigs: [[url: args]]])
   }
