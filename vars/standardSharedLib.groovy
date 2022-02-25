37aac306dafdd29a4cb474cd8af3e5fad30454ed
3ill <guiller.n93@gmail.com>
2022-02-25 09:59:59 +0100

    refactor

--- vars/standardSharedLib.groovy
+++ vars/standardSharedLib.groovy
@@ -12,98 +12,92 @@ def call(Map config){
         ARGOCDIP=credentials('argocdip')
     }
     stages {
-        if(config.steps.contains("checkout")){
-            stage('Checkout SMC') {
-                steps {
-                    checkout([$class: 'GitSCM', branches: [[name: 'master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/3illH/gs-rest-service-pipeline']]])
-                }
-            }
-        }
-        if(config.steps.contains("build")){
-            stage('Build') {
-                steps {
-                    container('maven') {
-                        script {
-                            pom = readMavenPom file: "pom.xml";
-                            sh "mvn clean package -DskipTests"
-                        }
-                    }
-                }
-            }
-        }
-        if(config.steps.contains("test")){
-            stage('Test') {
-                steps {
-                    container('maven') {
-                        script {
-                            sh "mvn clean verify"
-                            dependencyCheckPublisher pattern: 'target/dependency-check-report.xml'
-                        }
-                    }
-                }
-            }
-        }
-        if(config.steps.contains("sonar")){
-            stage('SonarQube Analysis') {
-                steps {
-                    container('maven') {
-                        withSonarQubeEnv('Sonar') {
-                            sh "mvn clean verify sonar:sonar -Dsonar.projectKey=gs-rest-service"
-                        }
-                    }
-                }
-            }
-        }
-        if(config.steps.contains("dockerBuild")){
-            stage('Build with Docker') {
-                steps {
-                    container('docker') {
-                        script{
-                            dockerImageName = "3ill/gs-rest-service:${pom.version}"
-                            dockerImage = docker.build("${dockerImageName}", ".")
-                        }
-                    }
-                }
-            }
-        }
-        if(config.steps.contains("trivy")){
-            stage('Trivy Scan Container image') {
-                steps {
-                    container('trivy') {
-                        script {
-                            FAILED_STAGE=env.STAGE_NAME
-                            sh "trivy image -f json -o trivy-results.json 3ill/gs-rest-service:0.0.2-SNAPSHOT"
-                        }
-                    }
-                }
-            }
-        }
-        if(config.steps.contains("dockerPush")){
-            stage('Push with Docker') {
-                steps {
-                    container('docker') {
-                        script{
-                            sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
-                            sh "docker push ${dockerImageName}"
-                        }
-                    }
-                }
-            }
-        }
-        if(config.steps.contains("argocd")){
-            stage('Deploy with ArgoCd') {
-                steps {
-                    container('argocd'){
-                        withCredentials([usernamePassword(credentialsId: 'argocd', passwordVariable: 'argopassword', usernameVariable: 'argousername')]) {
-                            sh "argocd login $ARGOCDIP --insecure --username=$argousername --password=$argopassword"
-                            sh "argocd app set  gs-rest-service --kustomize-image ${dockerImageName}"
-                            sh "argocd app sync gs-rest-service --force --prune"
-                            sh "argocd app wait gs-rest-service --timeout 600"
-                        }
-                    }
-                }
-            }
-        }
+        checkoutStage();
+        // if(config.steps.contains("build")){
+        //     stage('Build') {
+        //         steps {
+        //             container('maven') {
+        //                 script {
+        //                     pom = readMavenPom file: "pom.xml";
+        //                     sh "mvn clean package -DskipTests"
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        // if(config.steps.contains("test")){
+        //     stage('Test') {
+        //         steps {
+        //             container('maven') {
+        //                 script {
+        //                     sh "mvn clean verify"
+        //                     dependencyCheckPublisher pattern: 'target/dependency-check-report.xml'
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        // if(config.steps.contains("sonar")){
+        //     stage('SonarQube Analysis') {
+        //         steps {
+        //             container('maven') {
+        //                 withSonarQubeEnv('Sonar') {
+        //                     sh "mvn clean verify sonar:sonar -Dsonar.projectKey=gs-rest-service"
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        // if(config.steps.contains("dockerBuild")){
+        //     stage('Build with Docker') {
+        //         steps {
+        //             container('docker') {
+        //                 script{
+        //                     dockerImageName = "3ill/gs-rest-service:${pom.version}"
+        //                     dockerImage = docker.build("${dockerImageName}", ".")
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        // if(config.steps.contains("trivy")){
+        //     stage('Trivy Scan Container image') {
+        //         steps {
+        //             container('trivy') {
+        //                 script {
+        //                     FAILED_STAGE=env.STAGE_NAME
+        //                     sh "trivy image -f json -o trivy-results.json 3ill/gs-rest-service:0.0.2-SNAPSHOT"
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        // if(config.steps.contains("dockerPush")){
+        //     stage('Push with Docker') {
+        //         steps {
+        //             container('docker') {
+        //                 script{
+        //                     sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+        //                     sh "docker push ${dockerImageName}"
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        // if(config.steps.contains("argocd")){
+        //     stage('Deploy with ArgoCd') {
+        //         steps {
+        //             container('argocd'){
+        //                 withCredentials([usernamePassword(credentialsId: 'argocd', passwordVariable: 'argopassword', usernameVariable: 'argousername')]) {
+        //                     sh "argocd login $ARGOCDIP --insecure --username=$argousername --password=$argopassword"
+        //                     sh "argocd app set  gs-rest-service --kustomize-image ${dockerImageName}"
+        //                     sh "argocd app sync gs-rest-service --force --prune"
+        //                     sh "argocd app wait gs-rest-service --timeout 600"
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
     }
     // post {
     //     always {
