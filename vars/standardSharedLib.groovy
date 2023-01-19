1f51d1daa4d58ece28944e5534fffb6f01cda51b
3ill <guiller.n93@gmail.com>
2023-01-19 09:18:19 +0100

    fix stage condition to add when

--- vars/standardSharedLib.groovy
+++ vars/standardSharedLib.groovy
@@ -17,30 +17,31 @@ def call(Map config){
                 checkoutStage(config)
             }
         }
-        if(config.steps.contains("build")){
-            stage('Build') {
-                steps {
-                    container('maven') {
-                        script {
-                            pom = readMavenPom file: "pom.xml";
-                            sh "mvn clean package -DskipTests"
-                        }
+
+        stage('Build') {
+            when { expression { return config.steps.contains("build") } }
+            steps {
+                container('maven') {
+                    script {
+                        pom = readMavenPom file: "pom.xml";
+                        sh "mvn clean package -DskipTests"
                     }
                 }
             }
         }
-        if(config.steps.contains("test")){
-            stage('Test') {
-                steps {
-                    container('maven') {
-                        script {
-                            sh "mvn clean verify"
-                            dependencyCheckPublisher pattern: 'target/dependency-check-report.xml'
-                        }
+
+        stage('Test') {
+            when { expression { return config.steps.contains("test") } }
+            steps {
+                container('maven') {
+                    script {
+                        sh "mvn clean verify"
+                        dependencyCheckPublisher pattern: 'target/dependency-check-report.xml'
                     }
                 }
             }
         }
+
         // if(config.steps.contains("sonar")){
         //     stage('SonarQube Analysis') {
         //         steps {
@@ -52,30 +53,32 @@ def call(Map config){
         //         }
         //     }
         // }
-        if(config.steps.contains("dockerBuild")){
-            stage('Build with Docker') {
-                steps {
-                    container('docker') {
-                        script{
-                            dockerImageName = "3ill/gs-rest-service:${pom.version}"
-                            dockerImage = docker.build("${dockerImageName}", ".")
-                        }
+
+        stage('Build with Docker') {
+            when { expression { return config.steps.contains("dockerBuild") } }
+            steps {
+                container('docker') {
+                    script{
+                        dockerImageName = "3ill/gs-rest-service:${pom.version}"
+                        dockerImage = docker.build("${dockerImageName}", ".")
                     }
                 }
             }
         }
-        if(config.steps.contains("trivy")){
-            stage('Trivy Scan Container image') {
-                steps {
-                    container('trivy') {
-                        script {
-                            FAILED_STAGE=env.STAGE_NAME
-                            sh "trivy image -f json -o trivy-results.json 3ill/gs-rest-service:0.0.2-SNAPSHOT"
-                        }
+
+
+        stage('Trivy Scan Container image') {
+            when { expression { return config.steps.contains("trivy") } }
+            steps {
+                container('trivy') {
+                    script {
+                        FAILED_STAGE=env.STAGE_NAME
+                        sh "trivy image -f json -o trivy-results.json 3ill/gs-rest-service:0.0.2-SNAPSHOT"
                     }
                 }
             }
         }
+
         // if(config.steps.contains("dockerPush")){
         //     stage('Push with Docker') {
         //         steps {
